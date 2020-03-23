c91fb37d9537ba5c0dce136c323b7b8776956787
Meredith Espinosa <goclonefilms@gmail.com>
2020-03-23 09:02:08 -0700

    put the renaming code back in oops

--- src/main/java/space/bbkr/endershulkers/mixin/MixinItemStack.java
+++ src/main/java/space/bbkr/endershulkers/mixin/MixinItemStack.java
@@ -2,6 +2,7 @@ package space.bbkr.endershulkers.mixin;
 
 import net.fabricmc.fabric.api.util.NbtType;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.DyeableItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
@@ -15,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import space.bbkr.endershulkers.EnderShulkers;
 
+import java.util.UUID;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack {
@@ -22,6 +24,12 @@ public abstract class MixinItemStack {
 	@Shadow public abstract Item getItem();
 
 	@Shadow public abstract CompoundTag getOrCreateTag();
+
+	@Shadow public abstract CompoundTag getOrCreateSubTag(String key);
+
+	@Shadow public abstract boolean hasCustomName();
+
+	@Shadow public abstract Text getName();
 
 	@Inject(method = "setCustomName", at = @At("HEAD"))
 	private void markNameDirty(Text name, CallbackInfoReturnable<ItemStack> info) {
@@ -33,6 +41,18 @@ public abstract class MixinItemStack {
 	//could theoretically go on the item class itself but I'm dealing with it in this mixin already so oh well
 	@Inject(method = "inventoryTick", at = @At("HEAD"))
 	private void renameEnderShulker(World world, Entity entity, int slot, boolean selected, CallbackInfo info) {
-
+		if (world.isClient) return;
+		if (getItem() == EnderShulkers.ENDER_SHULKER_ITEM) {
+			CompoundTag tag = getOrCreateTag();
+			if (tag.contains("DirtyName") && hasCustomName()) {
+				tag.remove("DirtyName");
+				CompoundTag beTag = getOrCreateSubTag("BlockEntityTag");
+				if (beTag.containsUuid("Owner")) {
+					int channel = ((DyeableItem)getItem()).getColor((ItemStack)(Object)this);
+					UUID id = beTag.getUuid("Owner");
+					EnderShulkers.ENDER_SHULKER_COMPONENT.get(world.getLevelProperties()).getInventory(id, channel).setCustomName(getName());
+				}
+			}
+		}
 	}
 }
